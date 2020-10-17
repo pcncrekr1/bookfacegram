e1d688d133457bb600c17e00e31279c267a2283e
pcncrekr1 <pcncrekr1@gmail.com>
2020-10-17 15:54:53 +0900

    메인 서브페이지들에 header 추가
    헤더 fixed 처리 및 메인 서브페이지들에 header 추가(좌측메뉴 햄버거버튼, 앱로고, 카메라 버튼, 검색창, 메인 탭메뉴들 추가됨)

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,24 @@
 $(function(){
 
+    //hidden gnb toggle
+    var menu_left = $(".menu_left");                            // 햄버거 버튼
+    var gnb_hidden = $(".gnb_hidden");                          // 숨겨져 있는 gnb
+    var hidden_cover = $(".hidden_cover");                      // 숨겨져있는 gnb의 투명 배경
+
+    menu_left.click(function(){
+        gnb_hidden.animate({'left': 0}, 400);                   // left -100% --> left 0 (나타나게 함)
+        hidden_cover.css('left', 0);                            // left -100%; --> left 0 (나타나게 함)
+        $('body').css('overflow-y', 'hidden');                  // 세로 스크롤 안되게 막음
+        event.preventDefault();
+    });
+    hidden_cover.click(function(){
+        gnb_hidden.animate({'left': '-' + 100 + '%'}, 400);     // left 0 --> left -100% (숨김)
+        hidden_cover.css('left', '-' + 100 + '%');              // left 0 --> left -100% (숨김)
+        $('body').css('overflow-y', 'scroll');                  // 세로 스크롤 되게 함
+        $('body').css('overflow-x', 'hidden');                  // 가로 스크롤이 생겨서 추가함
+    });
+    
+
     // 문해고사 정답 체크
     $(".exam_submit_btn").click(function () { 
         if($('input:radio[id=choice3]').is(':checked')) {
