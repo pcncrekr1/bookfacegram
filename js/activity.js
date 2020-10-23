bb22e9c20106ec52ced4d5a8d54c1eaf86f0ab9c
pcncrekr1 <pcncrekr1@gmail.com>
2020-10-23 17:23:07 +0900

    ACTIVITY 탭 화면 구현
    1. 기존 원형 프로그래스바 플러그인 삭제
    2. chart.js 플러그인 도넛 차트로 원형 프로그래스바 구현
    3. 불필요한 코드 삭제

--- js/activity.js
+++ js/activity.js
@@ -19,18 +19,113 @@ $(function(){
     });
 
 
-    // circleProgress
-    $('#circleProgress').circleProgress({
-        value: 0.8,
-        size: 200,
-        startAngle: -Math.PI / 2,
-        thickness: 10,
-        animation: false,
-        emptyFill: "rgba(0,0,0,0)",
-        fill: {
-            gradient: [['#31A6DB', 0], ['#298EBC', 0.2], ['#31A6DB', 0.4], ['#298EBC', 0.6], ['#31A6DB', 0.8], ['#298EBC', 1]], 
-            gradientAngle: Math.PI / 2
+
+    // 도넛 차트
+    var data1 = {
+        labels: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M"],
+        datasets: [{
+            data: [8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333],
+            backgroundColor: ['#FFE876', '#FFE35B', '#FED710', '#CCEC97', '#B7E664', '#8FCB2B', '#F15151', '#DE3838', '#DB1B1A', '#98D4EC', '#78CBEB', '#45B1D8']
+        }]
+    };
+    var data2 = {
+        labels: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M"],
+        datasets: [{
+            data: [8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333],
+            backgroundColor: ['#FFE876', '#FFE35B', '#FED710', '#CCEC97', '#B7E664', '#8FCB2B', '#F15151', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)']
+        }]
+    };
+    var data3 = {
+        labels: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M"],
+        datasets: [{
+            data: [8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333],
+            backgroundColor: ['#FFE876', '#FFE35B', '#FED710', '#CCEC97', '#B7E664', '#8FCB2B', '#F15151', '#DE3838', '#DB1B1A', '#98D4EC', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)']
+        }]
+    };
+    var data4 = {
+        labels: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M"],
+        datasets: [{
+            data: [8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333, 8.333],
+            backgroundColor: ['#FFE876', '#FFE35B', '#FED710', '#CCEC97', '#B7E664', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)', 'rgba(0,0,0,0)']
+        }]
+    };
+
+    var doughnutChart1 = new Chart(circleProgress1, {
+        type: 'doughnut',
+        data: data1,
+        options: {
+            legend: {
+                display: false
+            },
+            responsive: true,
+            cutoutPercentage: 80,
+            animation: false,
+            tooltips: false,
+            elements: {
+                arc: {
+                    borderWidth: 0
+                }
+            }
         }
     });
+
+    var doughnutChart2 = new Chart(circleProgress2, {
+        type: 'doughnut',
+        data: data2,
+        options: {
+            legend: {
+                display: false
+            },
+            responsive: true,
+            cutoutPercentage: 80,
+            animation: false,
+            tooltips: false,
+            elements: {
+                arc: {
+                    borderWidth: 0
+                }
+            }
+        }
+    });
+
+    var doughnutChart3 = new Chart(circleProgress3, {
+        type: 'doughnut',
+        data: data3,
+        options: {
+            legend: {
+                display: false
+            },
+            responsive: true,
+            cutoutPercentage: 80,
+            animation: false,
+            tooltips: false,
+            elements: {
+                arc: {
+                    borderWidth: 0
+                }
+            }
+        }
+    });
+
+    var doughnutChart4 = new Chart(circleProgress4, {
+        type: 'doughnut',
+        data: data4,
+        options: {
+            legend: {
+                display: false
+            },
+            responsive: true,
+            cutoutPercentage: 80,
+            animation: false,
+            tooltips: false,
+            elements: {
+                arc: {
+                    borderWidth: 0
+                }
+            }
+        }
+    });
+
+
 
 });
