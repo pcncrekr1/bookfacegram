fd592b80cbdb7e2dbe6bd906a57e6a889499bf14
pcncrekr1 <pcncrekr1@gmail.com>
2020-11-08 14:00:26 +0900

    피드북 갤러리 확대/축소 기능 구현 완료
    피드북 갤러리 확대/축소 기능 구현 완료

--- js/pinch_zoom.js
+++ js/pinch_zoom.js
@@ -1,56 +1,4 @@
 $(function() { 
-    // var element = document.querySelector('.feedbook_tab_content');
-    // var instance = panzoom(element, {
-    //     maxZoom: 1,
-    //     minZoom: 0.1,
-    //     bounds: true,
-    //     boundsPadding: 0.1
-    // });
-    // instance.on('zoom', function(e) {
-    //     console.log('Fired when `element` is zoomed', e);
-    //     // Note: e === instance.
-    // });
-
-
-
-
-
-
-    // $(".feedbook_tab_content").swipe( {
-    //     pinchStatus:function(event, phase, direction, distance , duration , fingerCount, pinchZoom, fingerData) {
-    //         // $(this).html("Pinch zoom " + pinchZoom + "  <br/>Distance pinched " + distance +" <br/>Direction " + direction);
-    //         console.log(direction);
-    //         if(direction == "in") {
-
-    //         }
-    //     },
-    //     fingers:2,
-    //     threshold:0,
-    //     preventDefaultEvents:false
-    // });
-
-
-
-    // $(".feedbook_tab_content").swipe( {
-    //     pinchIn:function(event, direction, distance, duration, fingerCount, pinchZoom)
-    //     {
-    //         $(this).html("You pinched " +direction + " by " + distance +"px, zoom scale is "+pinchZoom);
-    //     },
-    //     pinchOut:function(event, direction, distance, duration, fingerCount, pinchZoom)
-    //     {
-    //         $(this).html("You pinched " +direction + " by " + distance +"px, zoom scale is "+pinchZoom);
-    //     },
-    //     pinchStatus:function(event, phase, direction, distance , duration , fingerCount, pinchZoom) {
-    //         // $(this).html("Pinch zoom scale "+pinchZoom+"  <br/>Distance pinched "+distance+" <br/>Direction " + direction);
-    //     },
-    //     fingers:2,  
-    //     pinchThreshold:0  
-    // });
-
-
-
-
-
 
     var element = document.querySelector('.feedbook_tab_content');
 
@@ -62,38 +10,40 @@ $(function() {
         touchAction: ""
     });
 
-    // element.addEventListener('panzoomstart', function(event) {
-        
-    // });
-
-    element.addEventListener('panzoomzoom', function(event) {
-        $(".feedbook_tab_content").children().children().css("width", "calc(25% - 2px)");
+    var zoomCount = 0;
+    element.addEventListener('panzoomstart', function(event) {
+        $(".feedbook_tab_content").swipe( {
+            // pinchIn과 pinchOut이 반대로 되어있음.
+            // 축소하면 pinchOut이 발생하고 확대하면 pinchIn이 발생함.
+            pinchIn:function(event, direction, distance, duration, fingerCount, pinchZoom) {
+                // $(this).html("You pinched IN!!! " +direction + " by " + distance +"px, zoom scale is "+pinchZoom);
+                // 확대시
+                if( zoomCount === 0 ) { //원상태라면
+                    $(".feedbook_tab_content").children().children().css("width", "calc(50% - 2px)");
+                    zoomCount = 1;
+                } else if( zoomCount === -1 ) { //축소된 상태라면
+                    $(".feedbook_tab_content").children().children().css("width", "calc(33.3% - 2px)");
+                    zoomCount = 0;
+                }
+            },
+            pinchOut:function(event, direction, distance, duration, fingerCount, pinchZoom) {
+                // $(this).html("You pinched OUT!!! " +direction + " by " + distance +"px, zoom scale is "+pinchZoom);
+                // 축소시
+                if( zoomCount === 0 ) { //원상태라면
+                    $(".feedbook_tab_content").children().children().css("width", "calc(25% - 2px)");
+                    zoomCount = -1;
+                } else if( zoomCount === 1 ) { //확대된 상태라면
+                    $(".feedbook_tab_content").children().children().css("width", "calc(33.3% - 2px)");
+                    zoomCount = 0;
+                }
+            },
+            fingers:2,  
+            pinchThreshold:0  
+        });
     });
 
     element.addEventListener('panzoomend', function(event) {
-        // $(".feedbook_tab_content").swipe( {
-        //     pinchStatus:function(event, phase, direction, distance , duration , fingerCount, pinchZoom, fingerData) {
-        //         // $(this).html("Pinch zoom " + pinchZoom + "  <br/>Distance pinched " + distance +" <br/>Direction " + direction);
-        //         console.log(direction);
-        //     },
-        //     fingers:2,
-        //     threshold:0,
-        //     preventDefaultEvents: false
-        // });
-
-        // console.log(event);
-
+        // 축소시 원상태로 돌아오게 만듬
         panzoom.reset();
     });
-        
-
-
-    // 스크롤이 안되고 disablePan:true 적용시 zoom되 되지 않음
-    // var elem = document.querySelector('.feedbook_tab_content');
-    // var panzoom = Panzoom(elem, {
-    //     maxScale: 1,
-    //     minScale: 0.5,
-    //     touchAction: ""
-    // });
-
 });
